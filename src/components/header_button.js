535657fddec0a6d1b828a7efd64e4b509caffabe
Dary-V <darinabratchikova23@gmail.com>
2018-10-04 18:26:57 +0300

    unnecessary file

--- src/components/header_button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class HeaderButton extends React.Component {
-	render () {
-		return (
-           <button target='Button' value='Nea'>Click Me!</button>
-			);
-	}
-}
